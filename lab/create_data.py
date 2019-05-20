0d8d148145e4eb50795e9da7a59ae4732c9bd7ec
IkumaOka <ikuma1018soccer06@gmail.com>
2019-05-20 18:01:18 +0900

    .gitignore追加とcreate_data.pyの修正

--- lab/create_data.py
+++ lab/create_data.py
@@ -2,10 +2,7 @@ from sklearn.datasets.samples_generator import make_blobs
 import numpy as np
 from matplotlib import pyplot as plt
 
-dataset, y = make_blobs(centers=2)
-print(dataset.shape)
-print(y.shape)
-print(dataset[0])
-print(dataset[1])
-plt.scatter(dataset[:,0], dataset[:,1], c=y)
+dataset, features = make_blobs(centers=2, random_state=11)
+
+plt.scatter(dataset[:,0], dataset[:,1], c=features)
 plt.show()
