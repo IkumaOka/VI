d75b3b831288d988410500bddcbbb75e478d9ec7
IkumaOka <ikuma1018soccer06@gmail.com>
2019-06-09 00:12:24 +0900

    変分ベイズ実装

--- lab/va_ga_clusters.py
+++ lab/va_ga_clusters.py
@@ -13,15 +13,17 @@ def create_data(N, K):
         sigma_star = np.append(sigma_star, scale[i])
     return (X, mu_star, sigma_star)
 
+
 def gaussian(mu, sigma):
     def f(x):
         return np.exp(-0.5 * (x - mu) ** 2 / sigma) / np.sqrt(2 * np.pi * sigma)
     return f
 
+
 # 負担率を計算
 # ηを計算する式（式7.29参照）PRMLだと式10.46
 # 式7.29第1項において、多変量では正規-ウィシャート分布を使うが、一変量の場合は正規-ガンマ分布を使う
-def estimate_posterior_likelihood(X):
+def estimate_posterior_likelihood(X, psi, beta, kappa, xi, dir_param):
     # 正規-ガンマ分布の期待値を求める(wiki参照)
     ex_T = kappa / xi
     ex_T_X = psi * kappa / xi
@@ -32,7 +34,6 @@ def estimate_posterior_likelihood(X):
         eta.append(elm)
 
     eta = np.array(eta)
-    # print(eta)
     r = []
     for i in range(len(eta)):
         a = eta[i] / eta[i].sum()
@@ -40,11 +41,11 @@ def estimate_posterior_likelihood(X):
     r = np.array(r)
     return r
 
+
 # Mステップ
 def estimate_gmm_parameter(X, r, psi, beta, kappa, xi, dir_param):
     # 負担率の総和
     n_j = r.sum(axis=0)
-    print(n_j.shape)
     # 負担率による観測値の重み付き平均
     barx_j = np.dot(X.T, r) / n_j
 
@@ -58,6 +59,23 @@ def estimate_gmm_parameter(X, r, psi, beta, kappa, xi, dir_param):
     xi = ( n_j * barx_j + (n_j * beta + ((barx_j - psi) ** 2 / (n_j + beta))) / 2 ) + xi
     return psi, beta, kappa, xi, dir_param
 
+
+def normal_cluster(gamma):
+    return gamma
+
+
+def stochastic_cluster(gamma):
+    clusters = [0, 1]
+    stochastic_gamma = []
+    for i in range(len(gamma)):
+        p = np.random.choice(a=clusters, p=gamma[i])
+        a = np.zeros(len(gamma[i]))
+        a[p] = 1.0
+        stochastic_gamma.append(a)
+    stochastic_gamma = np.array(stochastic_gamma)
+    return stochastic_gamma
+
+
 def calc_log_likelihood(X, pi, gf):
     p_i = []
     for i in range(len(X)):
@@ -68,43 +86,45 @@ def calc_log_likelihood(X, pi, gf):
     log_p = np.log(p)
     return log_p
 
-np.random.seed(19)
-K = 2 # クラス数
-N = 1000 * K # データ数
-# π,μ,σの値を初期化
-pi = np.random.rand(K)
-mu = np.random.randn(K)
-sigma = np.abs(np.random.randn(K))
-lamda = 1.0 / sigma
-X, mu_star, sigma_star = create_data(N, K)
-# 正規-ガンマ分布のパラメータを定義
-psi = np.array([0.0, 0.0])
-beta = np.array([1.0, 0.9])
-kappa = np.array([1.0, 0.9])
-xi = np.array([1.0, 0.9])
-# ディリクレ分布のパラメータを定義
-dir_param = np.array([1.0, 0.1])
-log_likelihoods = []
-for iter in range(1000):
-    r = estimate_posterior_likelihood(X)
-    psi, beta, kappa, xi, dir_param = estimate_gmm_parameter(X, r, psi, beta, kappa, xi, dir_param)
-    # print("psi: ", psi)
-    # print("beta: ", beta)
-    # print("kappa: ", kappa)
-    # print("xi: ", xi)
-    # print("dir_param: ", dir_param)
-    # print()
-    ave_dir_param = np.average(dir_param)
-    # print("ave_dir_param: ", ave_dir_param)
-    ave_sigma = xi / kappa # 後でσを使うから期待値の逆数をとった
-    gf = gaussian(psi, ave_sigma)
-    log_likelihood = calc_log_likelihood(X, ave_dir_param, gf)
-    print(log_likelihood)
-    log_likelihoods.append(log_likelihood)
 
-log_likelihoods = np.array(log_likelihoods)
+def clustering(allocation=normal_cluster):
+    np.random.seed(19)
+    K = 2 # クラス数
+    N = 1000 * K # データ数
+    pi = np.random.rand(K)
+    mu = np.random.randn(K)
+    sigma = np.abs(np.random.randn(K))
+    lamda = 1.0 / sigma
+    X, mu_star, sigma_star = create_data(N, K)
+    # 正規-ガンマ分布のパラメータを定義
+    psi = np.array([0.0, 0.0])
+    beta = np.array([1.0, 0.9])
+    kappa = np.array([1.0, 0.9])
+    xi = np.array([1.0, 0.9])
+    # ディリクレ分布のパラメータを定義
+    dir_param = np.array([1.0, 0.1])
+    log_likelihoods = []
+    for iter in range(1000):
+        gamma = estimate_posterior_likelihood(X, psi, beta, kappa, xi, dir_param)
+        if iter != 0 and iter != 1:
+            r = allocation(gamma)
+        psi, beta, kappa, xi, dir_param = estimate_gmm_parameter(X, gamma, psi, beta, kappa, xi, dir_param)
+        ave_dir_param = np.average(dir_param)
+        ave_sigma = xi / kappa # 後でσを使うから期待値の逆数をとった
+        gf = gaussian(psi, ave_sigma)
+        log_likelihood = calc_log_likelihood(X, ave_dir_param, gf)
+        print(log_likelihood)
+        log_likelihoods.append(log_likelihood)
 
-plt.plot(log_likelihoods, color='#4169e1', linestyle='solid')
+    log_likelihoods = np.array(log_likelihoods)
+    return log_likelihoods
+
+
+va_ga_mix = clustering()
+va_ga_mix_stochastic = clustering(stochastic_cluster)
+
+plt.plot(va_ga_mix, color='#4169e1', linestyle='solid')
+plt.plot(va_ga_mix_stochastic, color='#f2410c', linestyle='dashdot')
 plt.show()
 
 
