733b3979b8f2f270b629799e825cdaf82ac6e105
IkumaOka <ikuma1018soccer06@gmail.com>
2019-06-27 13:30:11 +0900

    va_ga_mix_2.py修正

--- lab/va_ga_mix_2.py
+++ lab/va_ga_mix_2.py
@@ -6,7 +6,7 @@ from scipy.misc import logsumexp
 import math
 
 def create_data(N, K):
-    loc = np.array([1.0, 3.0]) # 平均の初期値
+    loc = np.array([1.0, 10.0]) # 平均の初期値
     scale = np.array([1.0, 2.0]) # 標準偏差の初期値
     X, mu_star, sigma_star = [], [], []
     for i in range(K):
@@ -23,7 +23,7 @@ def gaussian(mu, sigma):
 # 負担率を計算
 # ηを計算する式（式7.29参照）PRMLだと式10.46
 # 式7.29第1項において、多変量では正規-ウィシャート分布を使うが、一変量の場合は正規-ガンマ分布を使う
-def estimate_posterior_likelihood(X, psi, beta, kappa, xi, dir_param):
+def calc_r(X, psi, beta, kappa, xi, dir_param):
     # 正規-ガンマ分布の期待値を求める(wiki参照)
     ex_T = kappa / xi
     ex_T_X = psi * kappa / xi
@@ -50,7 +50,15 @@ def estimate_gmm_parameter(X, r, u_psi, u_beta, u_kappa, u_xi, u_dir_param):
     n_j = r.sum(axis=0)
     # 負担率による観測値の重み付き平均
     barx_j = np.dot(X.T, r) / n_j
+    # print(X.shape)
     # print(barx_j.shape)
+    # X_ = np.tile(X, (2, 1))
+    # print(X_.shape)
+    s = (np.tile(X, (2, 1)) - barx_j.reshape((2, 1)))
+    s_ = s**2
+    # 観測値の重み付き分散
+    s_j = (s_.T * r).sum(axis=0) / n_j
+    # print(s_j.shape)
 
     # q(π)のパラメータを更新(式7.44)
     u_dir_param = dir_param + n_j
@@ -59,19 +67,19 @@ def estimate_gmm_parameter(X, r, u_psi, u_beta, u_kappa, u_xi, u_dir_param):
     u_psi = ( n_j * barx_j + beta * psi ) / ( n_j + beta )
     u_beta = n_j + beta
     u_kappa = n_j / 2 + kappa
-    u_xi = ( n_j * barx_j + (n_j * beta + ((barx_j - psi) ** 2 / (n_j + beta))) / 2 ) + xi
+    u_xi = ( n_j * s_j + (n_j * beta * ((barx_j - psi) ** 2 / (n_j + beta))) ) / 2 + xi
     return u_psi, u_beta, u_kappa, u_xi, u_dir_param
 
 def calc_log_likelihood(X, u_psi, u_beta, u_kappa, u_xi, r):
     log_likelihood = 0.0
     for i in range(len(X)):
         diff = X[i] - psi
-        log_u = np.log(beta * diff**2 / (2 * (1 + beta)) + xi)
+        log_u = np.log(u_beta * diff**2 / (2 * (1 + u_beta)) + u_xi)
         elm1 = np.log(r)
-        elm2 = np.log(beta / (2 * math.pi * (1 + beta))) / 2
-        elm3 = kappa * np.log(xi)
-        elm4 = (kappa + 0.5) * log_u
-        elm5 = loggamma(kappa + 0.5) - loggamma(kappa)
+        elm2 = np.log(u_beta / (2 * math.pi * (1 + u_beta))) / 2
+        elm3 = u_kappa * np.log(u_xi)
+        elm4 = (u_kappa + 0.5) * log_u
+        elm5 = loggamma(u_kappa + 0.5) - loggamma(u_kappa)
         s = elm1 + elm2 + elm3 - elm4 + elm5
         # print(s)
         log_likelihood += logsumexp(s)
@@ -88,7 +96,7 @@ sigma = np.abs(np.random.randn(K))
 lamda = 1.0 / sigma
 X, mu_star, sigma_star = create_data(N, K)
 # 正規-ガンマ分布のパラメータを定義
-psi = np.array([-1.0, 1.0])
+psi = np.array([-1.0, 7.0])
 beta = np.array([1.0, 0.9])
 kappa = np.array([1.0, 0.9])
 xi = np.array([1.0, 0.9])
@@ -101,7 +109,7 @@ u_xi = xi
 u_dir_param = dir_param
 log_likelihoods = []
 for iter in range(1000):
-    r = estimate_posterior_likelihood(X, u_psi, u_beta, u_kappa, u_xi, u_dir_param)
+    r = calc_r(X, u_psi, u_beta, u_kappa, u_xi, u_dir_param)
     u_psi, u_beta, u_kappa, u_xi, u_dir_param = estimate_gmm_parameter(X, r, u_psi, u_beta, u_kappa, u_xi, u_dir_param)
     ave_dir_param = np.average(u_dir_param)
     ave_sigma = u_xi / u_kappa # 後でσを使うから期待値の逆数をとった
@@ -109,7 +117,7 @@ for iter in range(1000):
     log_likelihood = calc_log_likelihood(X, u_psi, u_beta, u_kappa, u_xi, r)
     # print(log_likelihood)
     log_likelihoods.append(log_likelihood)
-    # print(u_psi)
+    print(u_psi)
 
 log_likelihoods = np.array(log_likelihoods)
 
